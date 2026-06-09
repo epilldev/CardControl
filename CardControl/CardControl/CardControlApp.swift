f4e9f2bf60c36d44730d057a5ac42f3c130b40be
Felipe J. <35155805+epilldev@users.noreply.github.com>
2026-06-09 10:20:36 -0300

    chore(auth): configurar Firebase e Google Sign-In
    - Adiciona GoogleService-Info.plist
    - Instala FirebaseAuth
    - Instala GoogleSignIn
    - Configura inicialização do Firebase

--- CardControl/CardControl/CardControlApp.swift
+++ CardControl/CardControl/CardControlApp.swift
@@ -5,15 +5,29 @@
 
 import SwiftUI
 import CoreData
+import FirebaseCore
 
 @main
 struct CardControlApp: App {
-    let persistenceController = PersistenceController.shared
+
+    let persistenceController =
+        PersistenceController.shared
+
+    init() {
+
+        /// Inicializa os serviços do Firebase ao abrir o aplicativo.
+        FirebaseApp.configure()
+    }
 
     var body: some Scene {
+
         WindowGroup {
+
             SplashView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .environment(
+                    \.managedObjectContext,
+                    persistenceController.container.viewContext
+                )
         }
     }
 }

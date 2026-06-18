a9f0cdd68ed05a0f3294107891704ed2842ce327
Felipe J. <35155805+epilldev@users.noreply.github.com>
2026-06-18 08:29:56 -0300

    feat: Ajuste de estrutura do projeto

--- CardControl/CardControl/Core/AppRouter.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import SwiftUI
-import Combine
-
-/// Responsável pelo controle central de navegação do aplicativo.
-final class AppRouter: ObservableObject {
-
-    @Published var path = NavigationPath()
-}
